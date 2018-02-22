15e06ca0ae33062766e4c1b39ca3ee4d11723149
Sungwoo Nam <the.sungwoo@gmail.com>
2018-02-22 16:21:48 +0900

    udp comm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
 var log4js = require('log4js');
 var log = setupLog();
 
+const dgram = require('dgram');
+const comm = dgram.createSocket('udp4');
+
+comm.on('error', (err) => {
+  log.error(`comm error:\n${err.stack}`);
+  comm.close();
+});
+
+comm.on('message', (msg, rinfo) => {
+  var jm = JSON.parse(msg);
+  if( jm != null && jm.cmd == 'ioUpdate' )
+  {
+    log.info( `ioUpdate @${jm.address} : ${jm.value}` );
+  }
+  else
+  {
+    log.info(`comm got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  }
+});
+
+comm.on('listening', () => {
+  const address = comm.address();
+  log.info(`comm listening ${address.address}:${address.port}`);
+});
+
+comm.bind(41234);
+
+
+
 btnAutorun.onclick = (event) => { openTab(event, 'autorun'); log.debug('autorun clicked'); }
 btnManual.onclick = (event) => { openTab(event, 'manual'); log.debug('manual clicked'); }
 btnLog.onclick = (event) => { openTab(event, 'log'); log.debug('log clicked'); }
+
+btnRun.onclick = (event) =>  {  
+  comm.send( 
+    '"cmd":"runEquipment"', 
+    50000, 'localhost', 
+    (err) => 
+    { 
+      log.error("Failed to send runEquipment"); 
+    });
+}
+
+btnStop.onclick = (event) =>  {  
+  comm.send( 
+    '"cmd":"stopEquipment"', 
+    50000, '127.0.0.1', 
+    (err) => 
+    { 
+      log.error("Failed to send stopEquipment"); 
+    });
+}
 
 function openTab(evt, tabId) {
   var tabcontent = document.getElementsByClassName("tabcontent");
@@ -60,6 +109,6 @@ function setupLog() {
   });
   log4js.configure('./config/log4js.json');
 
-  var log = log4js.getLogger("log");
+  var log = log4js.getLogger("index");
   return log;
 }

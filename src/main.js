59d0fcb6688d313c0de301d962656862ce7b9400
Sungwoo Nam <the.sungwoo@gmail.com>
2018-02-27 15:42:32 +0900

    Update IO using CSF SampleStage

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800, height: 600,
     resizable: false,
-    movable: false,
+    //movable: false,
     minimizable: false,
     maximizable: false,
     fullscreen: false,

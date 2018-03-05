c72b9e4dc4a79aa0cb162e602afa8d918b1b8350
Sungwoo Nam <the.sungwoo@gmail.com>
2018-03-06 00:30:55 +0900

    tidyup

--- src/tab.js
+++ src/tab.js
@@ -1,22 +1,21 @@
 class Tab {
   constructor() {
+    this.content = document.getElementsByClassName("tabcontent");
+    this.links = document.getElementsByClassName("tablinks");
   }
 
   select(evt, tabId) {
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    for (var i = 0; i < this.content.length; i++) {
+      this.content[i].style.display = "none";
     }
 
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (var i = 0; i < this.links.length; i++) {
+      this.links[i].className = this.links[i].className.replace(" active", "");
     }
 
     document.getElementById(tabId).style.display = "block";
     evt.currentTarget.className += " active";
   }
-
 }
 
 module.exports = Tab;
